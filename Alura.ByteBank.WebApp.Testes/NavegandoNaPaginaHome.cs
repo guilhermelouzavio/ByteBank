1914f4fd67b63d1e5d7371e6ec7fced3a5fab793
Guilherme <guilherme.louzavio@hotmail.com>
2024-10-27 11:46:04 -0300

    subindo primeira versao do projeto

---
+++ Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.WebApp.Testes
+{
+    public class NavegandoNaPaginaHome
+    {
+        [Fact]
+        public void CrregaPaginaHomeEVerificarTituloPagina()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            driver.Navigate().GoToUrl("https://localhost:44309");
+
+            Assert.Contains("WebApp", driver.Title);
+        }
+
+    }
+}
